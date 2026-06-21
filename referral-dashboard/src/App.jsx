f7f64f23de83c6ffdfcaa009e2250d6a531cab47
Binusha <sriramuatluri@gmail.com>
2026-06-21 11:39:58 +0530

    fix login and rename vercel.json

--- referral-dashboard/src/App.jsx
+++ referral-dashboard/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Dashboard from "./pages/Dashboard";
 import ReferralDetails from "./pages/ReferralDetails";
 import NotFound from "./pages/NotFound";
